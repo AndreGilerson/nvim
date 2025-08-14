6f881ae52774ae780c058f19a3479ea23941aaf2
Andre Gilerson <Andre.Gilerson@ipt.fraunhofer.de>
2025-08-14 17:28:49 +0200

    Begin with custom nvim config
    Lazy.nvim as the plugin manager, some common options and kanagawa as a
    color scheme

---
+++ init.lua
@@ -0,0 +1,3 @@
+require("config.options") -- Options for nvim suchs as tab/space, hotkey timeout etc.
+require("config.lazy") -- config for lazy.nvim plugin manager, all plugins are then loaded through it
+require("config.keymaps") -- custom keymapings
