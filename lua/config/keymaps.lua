823a3af8506ccbfa972e2705b9b251b4d5a4e57d
Andre Gilerson <Andre.Gilerson@ipt.fraunhofer.de>
2025-08-15 16:35:31 +0200

    Add hotkeys for easier navigation

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,5 +10,17 @@ end
 -- Global, non plugin specific keymaps
 map("i", "jk", "<esc>", {desc = "Easier escape inside insert mode"})
 
+-- Easier window navigation, normal mode
+map("n", "<c-h>", "<c-w>h", {desc = "Easier window movement, to the left"})
+map("n", "<c-j>", "<c-w>j", {desc = "Easier window movement, to the down"})
+map("n", "<c-k>", "<c-w>k", {desc = "Easier window movement, to the up"})
+map("n", "<c-l>", "<c-w>l", {desc = "Easier window movement, to the right"})
+
+-- Easier window navigation, insert mode
+map("i", "<c-h>", "<esc><c-w>h", {desc = "Easier window movement, to the left"})
+map("i", "<c-j>", "<esc><c-w>j", {desc = "Easier window movement, to the down"})
+map("i", "<c-k>", "<esc><c-w>k", {desc = "Easier window movement, to the up"})
+map("i", "<c-l>", "<esc><c-w>l", {desc = "Easier window movement, to the right"})
+
 -- Nvim-tree specific
 map("n", "<c-e>", ":NvimTreeToggle<CR>")
