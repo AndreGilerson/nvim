1af79ae529af2a5d889eeedf9681f0c3531cafc6
Andre Gilerson <Andre.Gilerson@ipt.fraunhofer.de>
2025-08-15 17:05:24 +0200

    Add nvim-tree hotkeys

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,3 +1,18 @@
+local function on_attach(bufnr)
+    local api = require("nvim-tree.api")
+    api.config.mappings.default_on_attach(bufnr)
+
+    local function opts(desc)
+        return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+    end
+    
+    -- Define nvim-tree specific keymappings
+    vim.keymap.set("n", "l", api.node.open.edit, opts("Open"))
+    vim.keymap.set("n", "v", api.node.open.vertical, opts("Open in new vertical split"))
+    vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Close directory"))
+    vim.keymap.set("n", "<c-e>", ":NvimTreeToggle<CR>", opts("Toggle nvim-tre"))
+end
+
 
 return {
     "nvim-tree/nvim-tree.lua",
@@ -17,6 +32,7 @@ return {
             filters = {
                 dotfiles = true,
             },
+            on_attach = on_attach,
         }
     end,
 }
