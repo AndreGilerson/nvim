f3fe114cdbbe2bd79949c38f2e45f912399ea503
Andre Gilerson <andre.gilerson@gmail.com>
2026-05-01 17:07:27 +0200

    Enable esc and jk to quit terminal mode

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -14,6 +14,18 @@ return {
                 vim.cmd("setlocal winfixheight")
             end,
         })
+
+        -- Inside toggleterm buffers, let <Esc> and jk exit terminal mode.
+        -- Scoped to toggleterm only so other :terminal usage keeps Esc intact
+        -- for shell apps (fzf, less, vim-in-shell, etc.).
+        vim.api.nvim_create_autocmd("TermOpen", {
+            pattern = "term://*#toggleterm#*",
+            callback = function(args)
+                local opts = { buffer = args.buf }
+                vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+                vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+            end,
+        })
     end
 }
 
