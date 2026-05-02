33170165ac65b4a86a045456afc058b139fdd3a0
Andre Gilerson <andre.gilerson@gmail.com>
2026-05-02 20:05:45 +0200

    Move to main branch of treesitter and also ann markview

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,20 +1,37 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    lazy = false,
     build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
     config = function()
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = {
-                "lua", "vim", "vimdoc",
-                "nix",
-                "python",
-                "bash",
-                "json", "yaml", "toml",
-                "markdown", "markdown_inline",
-            },
-            auto_install = true,
-            highlight = { enable = true },
-            indent = { enable = true },
+        local parsers = {
+            "lua", "vim", "vimdoc",
+            "nix",
+            "python",
+            "bash",
+            "json", "yaml", "toml",
+            "markdown", "markdown_inline",
+        }
+
+        require("nvim-treesitter").install(parsers)
+
+        local filetypes = {
+            "lua", "vim", "help",
+            "nix",
+            "python",
+            "bash", "sh",
+            "json", "yaml", "toml",
+            "markdown",
+        }
+
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = filetypes,
+            callback = function(args)
+                local ok = pcall(vim.treesitter.start, args.buf)
+                if ok then
+                    vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                end
+            end,
         })
     end,
 }
